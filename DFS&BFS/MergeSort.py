5aa9b22c312d30748afc3ed2f6a34493e0f766e9
오예찬 <oycoyj2@gmail.com>
2023-07-14 20:09:47 +0900

    DFS&BFS

---
+++ DFS&BFS/MergeSort.py
@@ -0,0 +1,29 @@
+def Msort(lt, rt):
+    if lt < rt:
+        mid = (lt+rt)//2
+        Msort(lt, mid)
+        Msort(mid+1, rt)
+        p1 = lt
+        p2 = mid+1
+        tmp=[]
+        while p1 <= mid and p2 <= rt:
+            if arr[p1] < arr[p2]:
+                tmp.append(arr[p1])
+                p1 += 1
+            else:
+                tmp.append(arr[p2])
+                p2 += 1
+        if p1 <= mid: tmp = tmp + arr[p1: mid+1]
+        if p2 <= mid: tmp = tmp + arr[p2: rt+1]
+        for i in range(len(tmp)):
+            arr[lt + i] = tmp[i]
+
+
+if __name__ == "__main__":
+    arr = [23, 11, 45, 36, 15, 67, 33, 21]
+    print("Before sort:", end=' ')
+    print(arr)
+    Msort(0, 7)
+    print()
+    print("After sort: ", end='')
+    print(arr)
