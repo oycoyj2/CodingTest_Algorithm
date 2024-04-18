0ffd0977bc4161af5101d1704532760e9e7d46e4
oycoyj2 <oycoyj2@gmail.com>
2024-04-18 13:18:24 +0900

    Create postfix.py
    후위 표기식 문제 추가(스택)

---
+++ DoIt 알고리즘/1.자료구조/postfix.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin = open('input.txt', "rt")
+
+arr = input()
+stack = []
+res = ""
+
+for x in arr:
+    if x.isalnum():
+        res += x
+    else:
+        if x == '(':
+            stack.append(x)
+        elif x == '*' or x == '/':
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                res += stack.pop()
+            stack.append(x)
+        elif x == '+' or x == '-':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.append(x)
+        elif x == ')':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.pop()
+
+
+while stack:
+    res += stack.pop()
+
+print(res)
