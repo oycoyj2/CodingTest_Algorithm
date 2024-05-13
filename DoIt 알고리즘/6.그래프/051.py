4f8718b7ef9a2ea41e5150aed61d40b76cddb6e5
oycoyj2 <oycoyj2@gmail.com>
2024-05-13 19:25:52 +0900

    Create 051.py
    DoIt 알고리즘 51번 문제(해결 중)

---
+++ DoIt 알고리즘/6.그래프/051.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open("input.txt", 'rt')
+
+def find(a):
+    if a == parent[a]:
+        return a
+    else:
+        parent[a] = find(parent[a])
+        return parent[a]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a != b:
+        parent[b] = a
+
