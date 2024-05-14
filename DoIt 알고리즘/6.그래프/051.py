e5e005f571d182d87142911475a944f4569aee55
oycoyj2 <oycoyj2@gmail.com>
2024-05-14 17:11:29 +0900

    Update 051.py
    DoIt 알고리즘 51번 문제

--- DoIt 알고리즘/6.그래프/051.py
+++ DoIt 알고리즘/6.그래프/051.py
@@ -1,6 +1,10 @@
 import sys
 sys.stdin = open("input.txt", 'rt')
 
+N = int(input())
+M = int(input())
+dosi = [[0 for j in range(N+1)] for i in range(N+1)]
+
 def find(a):
     if a == parent[a]:
         return a
@@ -13,4 +17,37 @@ def union(a, b):
     b = find(b)
     if a != b:
         parent[b] = a
+
+for i in range(1, N+1):
+    dosi[i] = list(map(int, input().split()))
+    dosi[i].insert(0, 0)
+
+parent = [0] * (N+1)
+
+for i in range(1, N+1):
+    parent[i] = i
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if dosi[i][j] == 1:
+            union(i, j)
+
+route = list(map(int, input().split()))
+route.insert(0, 0)
+
+isConnect = True
+
+index = find(route[1])
+
+for i in range(2, len(route)):
+    if index != find(route[i]):
+        isConnect = False
+        break
+
+if isConnect == True:
+    print("YES")
+else:
+    print("NO")
+
+
 
