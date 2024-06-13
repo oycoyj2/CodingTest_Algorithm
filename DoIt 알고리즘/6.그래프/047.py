2d9f0f6dae87b510e5652c3287f98f15131d8de8
oycoyj2 <oycoyj2@gmail.com>
2024-06-13 21:26:09 +0900

    Create 047.py
    DoIt 알고리즘 47번 문제

---
+++ DoIt 알고리즘/6.그래프/047.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+sys.stdin = open("input.txt", 'rt')
+N, M = map(int, input().split())
+A = [[] for _ in range(N+1)]
+ans = [0] * (N+1)
+
+def BFS(v):
+    queue = deque()
+    queue.append(v)
+    visited[v] = True
+    while queue:
+        now_Node = queue.popleft()
+        for i in A[now_Node]:
+            if not visited[i]:
+                visited[i] = True
+                ans[i] += 1
+                queue.append(i)
+
+for i in range(M):
+    S, E = map(int, input().split())
+    A[S].append(E)
+
+for i in range(1, N+1):
+    visited = [False] * (N+1)
+    BFS(i)
+
+maxVal = 0
+for i in range(1, N+1):
+    maxVal = max(maxVal, ans[i])
+
+for i in range(1, N+1):
+    if ans[i] == maxVal:
+        print(i, end=" ")
+
+
+
