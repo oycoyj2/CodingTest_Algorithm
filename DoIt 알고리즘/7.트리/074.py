5dd7e602eb6ce2e86c3ff5f365d7f657737b7666
oycoyj2 <oycoyj2@gmail.com>
2024-07-12 23:38:59 +0900

    Create 074.py
    DoIt 알고리즘 74번 문제

---
+++ DoIt 알고리즘/7.트리/074.py
@@ -0,0 +1,56 @@
+import sys
+sys.stdin = open("input.txt", 'rt')
+from collections import deque
+print = sys.stdout.write
+N = int(input())
+tree = [[] for _ in range(N+1)]
+
+for _ in range(0, N-1):
+    s, e = map(int, input().split())
+    tree[s].append(e)
+    tree[e].append(s)
+
+depth = [0]*(N+1)
+parent = [0]*(N+1)
+visited = [False]*(N+1)
+
+def BFS(node):
+    queue = deque()
+    queue.append(node)
+    visited[node] = True
+    level = 1
+    now_size = 1
+    count = 0
+    while queue:
+        now_node = queue.popleft()
+        for next in tree[now_node]:
+            if not visited[next]:
+                visited[next] = True
+                queue.append(next)
+                parent[next] = now_node
+                depth[next] = level
+        count += 1
+        if count == now_size:
+            count = 0
+            now_size = len(queue)
+            level += 1
+
+BFS(1)
+
+def executeLCA(a, b):
+    if depth[a] < depth[b]:
+        a, b = b, a
+    while depth[a] != depth[b]:
+        a = parent[a]
+    while a != b:
+        a = parent[a]
+        b = parent[b]
+
+    return a
+
+M = int(input())
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    print(str(executeLCA(a, b)))
+    print("\n")
